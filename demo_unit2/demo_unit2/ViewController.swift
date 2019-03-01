0f38b1d0ccb237eee983584119a96f34b8ae4e27
MyLabWork <38661914+fansking@users.noreply.github.com>
2019-03-01 17:51:18 +0800

    simple homework unit 2

---
+++ demo_unit2/demo_unit2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  demo_unit2
+//
+//  Created by 客人用户 on 2019/3/1.
+//  Copyright © 2019 fansking. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

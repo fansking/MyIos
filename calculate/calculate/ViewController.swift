936cc8f6ad7a744a18d1e01a097dd440d5442725
MyLabWork <38661914+fansking@users.noreply.github.com>
2019-03-01 18:59:42 +0800

    update

--- calculate/calculate/ViewController.swift
+++ calculate/calculate/ViewController.swift
@@ -12,9 +12,11 @@ class ViewController: UIViewController {
     var isInputDigit=false
     var lastData:Double=0
     var optStr:String=" "
+    var optStr2:String=" "
     var curData:Double
     {
         get{
+            
             return Double.init(lbDisplayData.text!)!
         }
         set{
@@ -22,7 +24,14 @@ class ViewController: UIViewController {
         }
         
     }
+    
+    @IBOutlet weak var lbwarning: UILabel!
+    @IBOutlet weak var lbop: UILabel!
     @IBOutlet weak var lbDisplayData: UILabel!
+    @IBAction func clear(_ sender: UIButton) {
+        curData=0
+        isInputDigit=false
+    }
     @IBAction func appendDigit(_ sender: UIButton) {
         
         let digit=sender.currentTitle!
@@ -39,13 +48,54 @@ class ViewController: UIViewController {
         }
         
     }
+    @IBAction func addOpt(_ sender: UIButton) {
+        optStr2=sender.currentTitle!
+        lbop.text=optStr2
+        
+    }
     @IBAction func appendOpt(_ sender: UIButton) {
+        switch optStr2 {
+        case "√":
+            if curData<0{
+                lbwarning.text="负数不能开根号,请更换其他数字"
+            }else{
+                curData=sqrt(curData)
+                optStr2=" "
+                lbop.text=" "
+            }
+            
+        case "1/x":
+            curData=1/curData
+            optStr2=" "
+            lbop.text=" "
+        default:
+            break
+        }
         lastData=curData
         optStr=sender.currentTitle!
         isInputDigit=false
     }
     
     @IBAction func doOpt(_ sender: UIButton) {
+        switch optStr2 {
+        case "√":
+            if curData<0{
+                lbwarning.text="负数不能开根号,请更换其他数字"
+            }else{
+                curData=sqrt(curData)
+            }
+            
+        case "1/x":
+            if curData==0{
+                lbwarning.text="除数不能为0,请更换其他数字"
+            }else{
+                curData=1/curData
+                
+            }
+            
+        default:
+            break
+        }
         switch optStr {
         case "+":
             curData=curData+lastData
@@ -54,11 +104,21 @@ class ViewController: UIViewController {
         case "*":
             curData=lastData*curData
         case "/":
-            curData=lastData/curData
+            if curData==0{
+                lbwarning.text="除数不能为0,请更换其他数字"
+            }else{
+                curData=lastData/curData
+            }
+        
+        
         default:
             break
         }
+        optStr=" "
+        optStr2=" "
+        lbop.text=" "
     }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
